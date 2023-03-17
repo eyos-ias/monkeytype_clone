1bf0196afeaf957a185246c97457d5f2d053aece
eyos <eyosiastamirat20@gmail.com>
2023-03-17 14:12:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# another typing test
+
+![banner](assets/banner.png)
+
+Yet another typing test made in Flutter, because why not.
+
+Try it [here](http://type.jeffsieu.com/)
---
+++ lib/theme_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color green = Color(0xFF1A237E);
+  static const Color red = Colors.red;
+  static const Color yellow = Color(0xFF1A237E);
+}
